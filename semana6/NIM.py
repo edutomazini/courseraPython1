34b924b9117046a7c3f70fde71ffec684bb02c76
Eduardo <eduardo_tomazini@msn.com>
2020-01-21 17:27:54 -0300

    4 pontos

--- semana6/NIM.py
+++ semana6/NIM.py
@@ -1,86 +1,52 @@
 def partida():
-    global n,m
-    n = int(input("Quantas pecas? "))
-    m = int(input("Limite de pecas por jogada? "))
+    vezDe = ''
+    pecas = 0
+    n = int(input('Quantas pecas? '))
+    m = int(input('Limite de pecas por jogada? '))
     print('')
 
     if n%(m+1) == 0:
-        mudajogador = 0
         print('Você começa!')
         print('')
-        usuario_escolhe_jogada()
+        pecas = usuario_escolhe_jogada(n,m)
+        vezDe = 'O computador'
+
     else:
-        mudajogador = 1
         print('Computador começa!')
         print('')
-        computador_escolhe_jogada()
-   
-    while n > 0:
-        if mudajogador == 0:
-            mudajogador = 1
-            computador_escolhe_jogada()
-        else:
-            mudajogador = 0
-            usuario_escolhe_jogada()
- 
-def computador_escolhe_jogada():
-    global n,m
-    for i in range(1,n+1):
-        if (n-i)%(m+1)!=0:
-            p = n-i
-            if p == 1:
-                n = n - p
-                print('O computador tirou uma peça')
-                if (n == 1):
-                    print('Agora resta apenas uma peça no tabuleiro.')
-                    print('')
-                else:
-                    print('Agora restam',n,'peças no tabuleiro.')
-                    print('')
-                break  
-            else:
-                n = n - p
-                print('O computador tirou', p, 'pecas')
-                if (n == 1):
-                    print('Agora resta apenas uma peça no tabuleiro.')
-                    print('')
-                else:
-                    print('Agora restam',n,'peças no tabuleiro.')
-                    print('')
-                break
-        else:
-            p=1
-            n = n - p
-            print('O computador tirou', p, 'pecas')
-            if (n == 1):
-                print('Agora resta apenas uma peça no tabuleiro.')
-                print('')
-            else:
-                print('Agora restam',n,'peças no tabuleiro.')
-                print('')
-            break
- 
-def usuario_escolhe_jogada():
-    global n,m
-    p = int(input('Quantas peças você vai tirar? '))
+        pecas = computador_escolhe_jogada(n,m)
+        vezDe = 'Voce'
+    n = n-pecas
 
-    while (p>m or m-p < 0 or p <= 0):
+    print('n1',n)
+    print('pecas1', pecas)
+    print('vezDe1', vezDe)
+    print('-----------------')
+
+    while n > 0:
+        if vezDe == 'Voce':
+            pecas = usuario_escolhe_jogada(n,m)
+            vezDe = 'O computador'
+        else:
+            pecas = computador_escolhe_jogada(n,m)
+            vezDe = 'Voce'
+        n = n-pecas
+        print('n2',n)
+        print('pecas2', pecas)
+        print('vezDe2', vezDe)
+        print('-----------------')
+ 
+def computador_escolhe_jogada(n,m):
+    for i in range(1,n):
+        if (n-i)%(m+1)==0:
+            return i
+    return m
+ 
+def usuario_escolhe_jogada(n,m):
+    p = int(input('Quantas peças você vai tirar? '))
+    if (p>m or m-p < 0 or p <= 0):
         print('Oops! Jogada inválida! Tente de novo.')
-        p = int(input('Quantas peças você vai tirar? '))
-  
-    if p == 1:
-        print('Voce tirou uma peça')
-    else:
-        print('Você tirou', p, 'pecas')
-   
-    n = n - p
-    if (n == 1):
-        print('Agora resta apenas uma peça no tabuleiro.')
-        print('')
-    else:
-        print('Agora restam',n,'peças no tabuleiro.')
-        print('')
+        p = usuario_escolhe_jogada(n,m)
+    return p
 
 partida()
-print(n)
-print(m)
