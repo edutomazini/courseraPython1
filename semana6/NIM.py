81243a28c2bd21dd8ad05fb4ed070a60b0bf941d
Eduardo Correa <eduardo.correa@ipdv.mobi>
2020-01-22 00:25:00 -0300

    partida funcionando

--- semana6/NIM.py
+++ semana6/NIM.py
@@ -1,52 +1,60 @@
 def partida():
-    vezDe = ''
-    pecas = 0
-    n = int(input('Quantas pecas? '))
-    m = int(input('Limite de pecas por jogada? '))
-    print('')
+    n = int(input('Quantas peças? '))
+    m = int(input('Limite de peças por jogada? '))
 
-    if n%(m+1) == 0:
-        print('Você começa!')
-        print('')
-        pecas = usuario_escolhe_jogada(n,m)
-        vezDe = 'O computador'
+    if (m > n):
+        partida()
 
+    if n % (m+1) == 0:
+        print('\nVocê começa!')
+        vezDe = '\nVocê'
     else:
-        print('Computador começa!')
-        print('')
-        pecas = computador_escolhe_jogada(n,m)
-        vezDe = 'Voce'
-    n = n-pecas
-
-    print('n1',n)
-    print('pecas1', pecas)
-    print('vezDe1', vezDe)
-    print('-----------------')
+        print('\nComputador começa!')
+        vezDe = '\nO computador'
 
     while n > 0:
-        if vezDe == 'Voce':
-            pecas = usuario_escolhe_jogada(n,m)
-            vezDe = 'O computador'
+        if vezDe == '\nVocê':
+            pecas = usuario_escolhe_jogada(n, m)
         else:
-            pecas = computador_escolhe_jogada(n,m)
-            vezDe = 'Voce'
+            pecas = computador_escolhe_jogada(n, m)
+
         n = n-pecas
-        print('n2',n)
-        print('pecas2', pecas)
-        print('vezDe2', vezDe)
-        print('-----------------')
- 
-def computador_escolhe_jogada(n,m):
-    for i in range(1,n):
-        if (n-i)%(m+1)==0:
+
+        if pecas == 1:
+            print(vezDe, 'tirou uma peça.')
+        else:
+            print(vezDe, 'tirou', pecas, 'peças.')
+
+        if n == 1:
+            print('Agora resta apenas uma peça no tabuleiro.')
+        else:
+            if n == 0:
+                print('Fim do jogo! O computador ganhou!')
+            else:
+                print('Agora restam', n, 'peças no tabuleiro.')
+
+        if vezDe == '\nVocê':
+            vezDe = '\nO computador'
+        else:
+            vezDe = '\nVocê'
+
+
+def computador_escolhe_jogada(n, m):
+    for i in range(1, n):
+        if (n-i) % (m+1) == 0:
             return i
-    return m
- 
-def usuario_escolhe_jogada(n,m):
-    p = int(input('Quantas peças você vai tirar? '))
-    if (p>m or m-p < 0 or p <= 0):
-        print('Oops! Jogada inválida! Tente de novo.')
-        p = usuario_escolhe_jogada(n,m)
+    if m <= n:
+        return m
+    else:
+        return n
+
+
+def usuario_escolhe_jogada(n, m):
+    p = int(input('\nQuantas peças você vai tirar? '))
+    if (p > m or m-p < 0 or p <= 0):
+        print('\nOops! Jogada inválida! Tente de novo.')
+        p = usuario_escolhe_jogada(n, m)
     return p
+
 
 partida()
