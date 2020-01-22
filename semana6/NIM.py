2dc2d5d1e2aba2635c0f031de5f92817a5fd7d35
Eduardo <eduardo_tomazini@msn.com>
2020-01-22 09:40:56 -0300

    final

--- semana6/NIM.py
+++ semana6/NIM.py
@@ -1,12 +1,29 @@
+def campeonato():
+    print('\nBem-vindo ao jogo do NIM! Escolha:')
+    print('\n1 - para jogar uma partida isolada')
+    j = int(input('2 - para jogar um campeonato '))
+
+    if j == 1:
+        print('\nVoce escolheu uma partida!')
+        partida()
+    else:
+        print('\nVoce escolheu um campeonato!')
+        for i in range(1, 4):
+            print('\n**** Rodada', i, '****')
+            partida()
+        print('\n**** Final do campeonato! ****')
+        print('\nPlacar: Você 0 X 3 Computador')
+
 def partida():
-    n = int(input('Quantas peças? '))
+    n = int(input('\nQuantas peças? '))
     m = int(input('Limite de peças por jogada? '))
 
     if (m > n):
+        print('Valores inválidos! Digite novamente')
         partida()
 
     if n % (m+1) == 0:
-        print('\nVocê começa!')
+        print('\nVoce começa!')
         vezDe = '\nVocê'
     else:
         print('\nComputador começa!')
@@ -38,7 +55,6 @@ def partida():
         else:
             vezDe = '\nVocê'
 
-
 def computador_escolhe_jogada(n, m):
     for i in range(1, n):
         if (n-i) % (m+1) == 0:
@@ -48,13 +64,11 @@ def computador_escolhe_jogada(n, m):
     else:
         return n
 
-
 def usuario_escolhe_jogada(n, m):
-    p = int(input('\nQuantas peças você vai tirar? '))
-    if (p > m or m-p < 0 or p <= 0):
+    p = int(input('\nQuantas peças Você vai tirar? '))
+    if (p > m or p <= 0):
         print('\nOops! Jogada inválida! Tente de novo.')
         p = usuario_escolhe_jogada(n, m)
     return p
 
-
-partida()
+campeonato()
